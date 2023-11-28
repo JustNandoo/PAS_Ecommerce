20c5e37fd4628cde6220fdb409ca90b721e56dcd
JustNandoo <nandofajar1211@gmail.com>
2023-11-28 22:41:10 +0700

    [AF] Create Empty Cart

--- lib/Pages/bag_page/BagPage.dart
+++ lib/Pages/bag_page/BagPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/Pages/bag_page/bage_controller.dart';
 import 'package:flutter_ecommerce/component/bags.dart';
+import 'package:flutter_ecommerce/component/empty.dart';
 import 'package:flutter_ecommerce/component/myButton.dart';
 import 'package:flutter_ecommerce/pages/Navigation.dart';
 import 'package:get/get.dart';
@@ -12,59 +13,66 @@ class BagPage extends StatelessWidget {
 
   final BageController controller = Get.put(BageController());
 
+  Widget bagItemList(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                myText("Your Cart",
+                    style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  height: size.height * 0.9,
+                  width: size.width * 1.1,
+                  child: Obx(
+                    () => controller.isRefresh == true
+                        ? ListView.builder(
+                            itemCount: controller.responeBag.length,
+                            shrinkWrap: true,
+                            itemBuilder: (BuildContext context, int index) {
+                              return BagItem(
+                                  item: controller.responeBag[index]);
+                            },
+                          )
+                        : ListView.builder(
+                            itemCount: controller.responeBag.length,
+                            shrinkWrap: true,
+                            itemBuilder: (BuildContext context, int index) {
+                              return BagItem(
+                                  item: controller.responeBag[index]);
+                            },
+                          ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  myText("My Bag",
-                      style: TextStyle(
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black)),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    height: size.height * 0.9,
-                    width: size.width * 1.1,
-                    child: Obx(
-                      () => controller.isRefresh == true
-                          ? ListView.builder(
-                              itemCount: controller.responeBag.length,
-                              shrinkWrap: true,
-                              itemBuilder: (BuildContext context, int index) {
-                                return BagItem(
-                                    item: controller.responeBag[index]);
-                              },
-                            )
-                          : ListView.builder(
-                              itemCount: controller.responeBag.length,
-                              shrinkWrap: true,
-                              itemBuilder: (BuildContext context, int index) {
-                                return BagItem(
-                                    item: controller.responeBag[index]);
-                              },
-                            ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: Obx(() => controller.responeBag.value.length == 0
+          ? EmptyCart()
+          : bagItemList(context)),
       bottomNavigationBar: bottomNavbar(context, currentIndex),
     );
   }
